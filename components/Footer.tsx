f452b480edc0d7f77ddabd2c5fd010817cace285
Chaos <36864386@qq.com>
2024-08-04 18:30:37 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,10 @@ const Footer = () => {
   return (
     <div className="container flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
       <a href="/" className="flex items-center gap-2.5">
-        <Image
-          unoptimized
-          src="/new-logo.svg"
-          alt="footer"
-          width={160}
-          height={30}
-        />
+          云识教育
       </a>
       <div className="flex items-center gap-3">
-        <Link href={"https://x.com/nutlope"} target="_blank">
+        <Link href={"http:yunshi.tech"} target="_blank">
           <Image
             unoptimized
             src="/twitter.svg"
